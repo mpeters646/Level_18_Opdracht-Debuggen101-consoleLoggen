fe005b8ecd399d148821689da2590cc34e00d8bb
Marco Peters <m.peters646@gmail.com>
2022-01-07 12:25:32 +0100

    modified debug.js | translated to english

--- debug.js
+++ debug.js
@@ -1,20 +1,20 @@
 const array = [
-  { name: 'N. Armstrong', profession: 'spacecowboy', age: 89 },
-  { name: 'H. de Haan', profession: 'kippen hypnotiseur', age: 59 },
-  { name: 'A. Curry', profession: 'kikvorsman', age: 32 },
-  { name: 'F. Vonk', profession: 'slangenmelker', age: 36 },
-  { name: 'B. Bunny', profession: 'konijnen uitlaatservice', age: 27 },
-  { name: 'Dr.Evil', profession: 'digital overlord', age: 56 },
+  { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+  { name: "H. de Haan", profession: "chicken hypnotist", age: 59 },
+  { name: "A. Curry", profession: "frogman", age: 32 },
+  { name: "F. Vonk", profession: "snake milker", age: 36 },
+  { name: "B. Bunny", profession: "rabbit walking service", age: 27 },
+  { name: "Dr.Evil", profession: "digital overlord", age: 56 },
 ];
 
 for (let person of array) {
-  //hier moeten console.logs komen
-  // console.log('Dit is de gehele persoon:', person);
-  // console.log('Dit is naam:', person.name);
-  // console.log('Dit is geboortejaar:', 2021 - person.age);
-  // console.log(person.name + ' is een ' + person.profession);
+  // This is where the console.logs go
+  console.log("This is the whole person:", person);
+  console.log("This is the name:", person.name);
+  console.log("This is the year of birth:", 2021 - person.age);
+  console.log(person.name + " is a " + person.profession);
 
   if (person.age > 50) {
-    console.log(person.name + ' is ouder dan 50');
+    console.log(person.name + " is older than 50");
   }
 }
